08e9c37676ee03b93d2d284f06be3bc1ca923062
Vandal-William <vandal.william@hotmail.com>
2023-03-18 18:18:20 +0100

    all

--- main.py
+++ main.py
@@ -1,122 +1,88 @@
-import os
-import psycopg2
 from flask import Flask
-from flask import request
 from flask import render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-def get_db_connection():
-    connect = psycopg2.connect(host='localhost',
-                            database='flask_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return connect
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'flask'
+app.config['MYSQL_PASSWORD'] = 'flask'
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
 
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/database", methods=['GET'])
+@app.route("/database")
 def database():
-    if request.method == 'GET':
-        return render_template('database.html')
+    return render_template('database.html')
     
-@app.route("/database/ticketing", methods=['GET'])
-def ticketing():
-    if request.method == 'GET':
-        connect = get_db_connection()
-        cur = connect.cursor()
-        cur.execute('SELECT * FROM ticket;')
-        tickets = cur.fetchall()
-        print(tickets)
-        cur.close()
-        connect.close()
-        return render_template('ticketing.html', tickets=tickets)
+@app.route("/database/events")
+def event():
+    cursor = mysql.connection.cursor()
+    cursor.execute('select * from place;')
+    events = cursor.fetchall()
+    mysql.connection.commit()
+    cursor.close()
+    return render_template('events.html', events=events)
 
-@app.route("/database/events", methods=['GET'])
-def events():
-    if request.method == 'GET':
-        connect = get_db_connection()
-        cur = connect.cursor()
-        cur.execute('SELECT * FROM event;')
-        events = cur.fetchall()
-        print(events)
-        cur.close()
-        connect.close()
-        return render_template('events.html', events=events)
-
-@app.route("/database/musicalGroups", methods=['GET'])
-def musicalGroups():
-    if request.method == 'GET':
-        connect = get_db_connection()
-        cur = connect.cursor()
-        cur.execute('SELECT * FROM musicians;')
-        musicians = cur.fetchall()
-        print(musicians)
-        cur.close()
-        connect.close()
-        return render_template('musicalGroups.html', musicians=musicians)
+@app.route("/database/events/<id>")
+def eventDetails(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('select * from place;')
+    events = cursor.fetchall()
+    mysql.connection.commit()
+    cursor.close()
+    return render_template('events.html', events=events)
     
-@app.route("/database/eventsDashboard", methods=['GET'])
+@app.route("/database/eventsDashboard")
 def eventsDashboard():
-    if request.method == 'GET':
-        return render_template('eventsDashboard.html') 
+    return render_template('eventsDashboard.html') 
 
-@app.route("/database/contactMusician", methods=['GET'])
+@app.route("/database/contactMusician")
 def contactMusician():
-    if request.method == 'GET':
-        return render_template('contactMusician.html')
+    return render_template('contactMusician.html')
 
-@app.route("/database/addEvents", methods=['GET'])
+@app.route("/database/addEvents")
 def addEvents():
-    if request.method == 'GET':
-        return render_template('addEvents.html')
+    return render_template('addEvents.html')
 
-@app.route("/database/volunteers", methods=['GET'])
+@app.route("/database/volunteers")
 def volunteers():
-    if request.method == 'GET':
-        return render_template('volunteers.html')
+    return render_template('volunteers.html')
 
-@app.route("/database/spectators", methods=['GET'])
+@app.route("/database/spectators")
 def spectators():
-    if request.method == 'GET':
-        return render_template('spectators.html')
+    return render_template('spectators.html')
 
-@app.route("/database/concertHalls", methods=['GET'])
+@app.route("/database/concertHalls")
 def concertHalls():
-    if request.method == 'GET':
-        return render_template('concertHalls.html') 
+    return render_template('concertHalls.html') 
 
-@app.route("/database/constraint", methods=['GET'])
+@app.route("/database/constraint")
 def constraint():
-    if request.method == 'GET':
-        return render_template('constraint.html')
+    return render_template('constraint.html')
 
-@app.route("/database/contactOrganizer", methods=['GET'])
+@app.route("/database/contactOrganizer")
 def contactOrganizer():
-    if request.method == 'GET':
-        return render_template('contactOrganizer.html')
+    return render_template('contactOrganizer.html')
 
-@app.route("/database/todo-list", methods=['GET'])
+@app.route("/database/todo-list")
 def todoList():
-    if request.method == 'GET':
-        return render_template('todo-list.html')
+    return render_template('todo-list.html')
 
-@app.route("/database/validateParticipation", methods=['GET'])
+@app.route("/database/validateParticipation")
 def validateParticipation():
-    if request.method == 'GET':
-        return render_template('validateParticipation.html')
+    return render_template('validateParticipation.html')
 
-@app.route("/database/roomsAvailable", methods=['GET'])
+@app.route("/database/roomsAvailable")
 def roomsAvailable():
-    if request.method == 'GET':
-        return render_template('roomsAvailable.html')
+    return render_template('roomsAvailable.html')
 
-@app.route("/database/dashboard", methods=['GET'])
+@app.route("/database/dashboard")
 def dashboard():
-    if request.method == 'GET':
-        return render_template('dashboard.html')  
+    return render_template('dashboard.html')  
     
+
